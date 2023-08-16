0ae32b18434f00370f7eae797dc1eef79214d7b6
Oğuz Demirel <Oguzdmrl16@gmail.com>
2023-08-16 13:02:40 +0300

    UOW repo kullanımı için static sınıfım hazırlandı

---
+++ Business/Tools.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Context;
+using DataAccess.Repo.UnitOfWorks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business;
+
+public static class Tools
+{
+    static AppDBContext _dbContext = AppStatic.ServiceProviderInstance.GetRequiredService<AppDBContext>();
+    private static UnitOfWork uow;
+    public static UnitOfWork UOW
+    {
+        get
+        {
+            if (uow == null)
+            {
+                uow = new(_dbContext);
+            }
+            return uow;
+        }
+    }
+}
