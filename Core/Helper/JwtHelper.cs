770b04e3e8f200b60e9d713a7a15c85cbb83e4d7
Oğuz Demirel <Oguzdmrl16@gmail.com>
2023-08-16 13:01:37 +0300

    Jwt için helper hazırlandı

---
+++ Core/Helper/JwtHelper.cs
@@ -0,0 +1,32 @@
+﻿using Entities.JWTEntity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Helper;
+
+public class JwtHelper
+{
+    public static TokenModel GetJwtToken(JwtSettings jwtSettings, List<Claim> claims)
+    {
+        TokenModel TokenModel = new();
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SigningKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        TokenModel.Expiration = DateTime.UtcNow.AddMinutes(60);
+
+        JwtSecurityToken tokens = new
+            (
+              issuer: jwtSettings.Issuer,
+              audience: jwtSettings.Audience,
+              notBefore: DateTime.UtcNow,
+              expires: TokenModel.Expiration,
+              claims: claims,
+              signingCredentials: creds
+            );
+        TokenModel.Token = new JwtSecurityTokenHandler().WriteToken(tokens);
+        return TokenModel;
+    }
+}
