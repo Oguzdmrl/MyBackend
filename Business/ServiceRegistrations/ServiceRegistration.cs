e84c372bb65600070ed2d436ab823425874d6adb
Oğuz Demirel <Oguzdmrl16@gmail.com>
2023-08-16 13:03:16 +0300

    Servis Registration için configurasyon hazırlandı

---
+++ Business/ServiceRegistrations/ServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Business.Repo.BaseRepo;
+using DataAccess.Context;
+using DataAccess.Repo.UnitOfWorks;
+using DataAccess.UnitOfWorks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.ServiceRegistrations
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddEndpointsApiExplorer();
+            services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork));
+            services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>)); // Düzenlenicek
+            services.AddTransient(typeof(BaseRepository<>)); // Düzenlenicek
+         
+            services.AddHttpContextAccessor();
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddDbContext<AppDBContext>(x => x.UseNpgsql(configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Scoped);
+
+            return services;
+        }
+    }
+}
